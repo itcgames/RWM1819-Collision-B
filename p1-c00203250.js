6799a8d2cb4f104bf1d0f963d16d07e31e765e66
Rafael Plugge <rafael.plugge@email.com>
2018-11-02 14:10:17 +0000

    Added appropriate comments on private functions.

--- p1-c00203250.js
+++ p1-c00203250.js
@@ -2,20 +2,53 @@
 "use strict";
 
 const collisionManager = (function () {
+
+  /**
+   * Type check that the passed in parameter is a object and 
+   * contains the necessary properties to be considered a vector.
+   * @param {{ x: number, y: number}} vector
+   *  2 dimensional vector.
+   */
   function isVector(vector) {
     return typeof vector === "object" &&
       typeof vector.x === "number" && typeof vector.y === "number";
   };
+
+  /**
+   * Type check that the passed in parameter is a object and
+   * contains the necessary properties to be considered a circle.
+   * @param {{ position: {x: number, y: number}, radius: number}} circle
+   *  circle represented by a position and a radius
+   */
   function isCircle(circle) {
     return typeof circle === "object" && isVector(circle.position) &&
       typeof circle.radius === "number";
   };
+
+  /**
+   * calculate the length of the vector squared,
+   * used to avoid the lengthly operation of square rooting.
+   * @param {{ x: number, y: number }} vector
+   *  2 dimensional vector
+   */
   function lengthSquared(vector) {
     return (vector.x * vector.x) + (vector.y * vector.y);
   };
+
+  /**
+   * calculate the length of the vector.
+   * @param {{ x: number, y: number }} vector 
+   *  2 dimensional vector
+   */
   function length(vector) {
     return Math.sqrt(lengthSquared(vector));
   };
+
+  /**
+   * calculate the unit vector.
+   * @param {{ x: number, y: number }} vector 
+   *  2 dimensional vector
+   */
   function unit(vector) {
     const magnitude = length(vector);
     if (magnitude === 0) {
@@ -24,10 +57,12 @@ const collisionManager = (function () {
     return { x: vector.x / magnitude, y: vector.y / magnitude };
   };
 
+
   /**
-   * 
-   * @param {{ position: {x: number, y: number}, radius: number }} leftCircle left circle.
-   * @param {{ position: {x: number, y: number}, radius: number }} rightCircle right circle.
+   * @param {{ position: {x: number, y: number}, radius: number }} leftCircle
+   *  left circle.
+   * @param {{ position: {x: number, y: number}, radius: number }} rightCircle
+   *  right circle.
    * @returns {boolean} true if circle has collided.
    */
   function boolCircleToCircle(leftCircle, rightCircle) {
@@ -45,10 +80,16 @@ const collisionManager = (function () {
 
   /**
    * 
-   * @param {{ position: {x: number, y: number}, radius: number }} leftCircle left circle.
-   * @param {{ position: {x: number, y: number}, radius: number }} rightCircle right circle.
-   * @returns {{ collision: boolean, distance: {x: number, y: number} }} object 
-   * containing the property 'distance' and 'collision', the latter containing whether a collision occurred.
+   * @param {{ position: {x: number, y: number}, radius: number }} leftCircle
+   *  left circle.
+   * @param {{ position: {x: number, y: number}, radius: number }} rightCircle
+   *  right circle.
+   * @returns {{ collision: boolean, distance: {x: number, y: number} }}
+   *  object containing the property 'manifest' and 'collision',
+   *  the latter containing whether a collision occurred,
+   *  the first one containing the property 'leftCircleDistance' and
+   *  'rightCircleDistance' each containing the amount you can push each
+   *  circle away from each other.
    */
   function maniCircleToCircle(leftCircle, rightCircle) {
     if (!isCircle(leftCircle) || !isCircle(rightCircle)) {
@@ -60,9 +101,10 @@ const collisionManager = (function () {
       y: rightCircle.position.y - leftCircle.position.y
     };
 
-    const result = lengthSquared(distanceBetween) - Math.pow(leftCircle.radius + rightCircle.radius, 2);
+    const result = lengthSquared(distanceBetween) -
+        Math.pow(leftCircle.radius + rightCircle.radius, 2);
     if (0 > result) {
-      return { collision: false, distance: { x: 0, y: 0 } };
+      return { collision: false, manifest: {} };
     }
 
     // collision occurred generate manifest
