5f579b25770989f31a1927b475d5f2eb4d3ecd5a
Rafael Plugge <rafael.plugge@email.com>
2018-11-02 13:54:46 +0000

    Re-formated script and test to abide by the new coding standard.

--- p1-c00203250.js
+++ p1-c00203250.js
@@ -1,78 +1,88 @@
 /*! p1-c00203250 v0.0.0 - MIT license */
 "use strict";
 
-var collisionManager = (function() {
-    var isVector = function (vector) {
-        return typeof vector === "object" &&
-            typeof vector.x === "number" && typeof vector.y === "number";
-    };
-    var isCircle = function (circle) {
-        return typeof circle === "object" && isVector(circle.position) &&
-            typeof circle.radius === "number";
-    };
-    var lengthSquared = function (vector) {
-        return (vector.x * vector.x) + (vector.y * vector.y);
-    };
-    var length = function (vector) {
-        return Math.sqrt(lengthSquared(vector));
-    };
-    var unit = function (vector) {
-        var magnitude = length(vector);
-        if (magnitude === 0) { return { x: 0, y: 0 }; }
-        return { x: vector.x / magnitude, y: vector.y / magnitude };
+const collisionManager = (function () {
+  function isVector(vector) {
+    return typeof vector === "object" &&
+      typeof vector.x === "number" && typeof vector.y === "number";
+  };
+  function isCircle(circle) {
+    return typeof circle === "object" && isVector(circle.position) &&
+      typeof circle.radius === "number";
+  };
+  function lengthSquared(vector) {
+    return (vector.x * vector.x) + (vector.y * vector.y);
+  };
+  function length(vector) {
+    return Math.sqrt(lengthSquared(vector));
+  };
+  function unit(vector) {
+    const magnitude = length(vector);
+    if (magnitude === 0) {
+      return { x: 0, y: 0 };
+    }
+    return { x: vector.x / magnitude, y: vector.y / magnitude };
+  };
+
+  /**
+   * 
+   * @param {{ position: {x: number, y: number}, radius: number }} leftCircle left circle.
+   * @param {{ position: {x: number, y: number}, radius: number }} rightCircle right circle.
+   * @returns {boolean} true if circle has collided.
+   */
+  function boolCircleToCircle(leftCircle, rightCircle) {
+    if (!isCircle(leftCircle) || !isCircle(rightCircle)) {
+      throw "Invalid parameter";
+    }
+
+    const distanceBetween = {
+      x: leftCircle.position.x - rightCircle.position.x,
+      y: leftCircle.position.y - rightCircle.position.y
     };
 
-    /**
-     * 
-     * @param {{ position: {x: number, y: number}, radius: number }} leftCircle left circle.
-     * @param {{ position: {x: number, y: number}, radius: number }} rightCircle right circle.
-     * @returns {boolean} true if circle has collided.
-     */
-    var boolCircleToCircle = function (leftCircle, rightCircle) {
-        if (!isCircle(leftCircle) || !isCircle(rightCircle)) { throw "Invalid parameter"; }
+    return (Math.pow(leftCircle.radius + rightCircle.radius, 2) > lengthSquared(distanceBetween));
+  };
 
-       var distanceBetween = {
-           x: leftCircle.position.x - rightCircle.position.x,
-           y: leftCircle.position.y - rightCircle.position.y
-       };
-       
-       return (Math.pow(leftCircle.radius + rightCircle.radius, 2) > lengthSquared(distanceBetween));
+  /**
+   * 
+   * @param {{ position: {x: number, y: number}, radius: number }} leftCircle left circle.
+   * @param {{ position: {x: number, y: number}, radius: number }} rightCircle right circle.
+   * @returns {{ collision: boolean, distance: {x: number, y: number} }} object 
+   * containing the property 'distance' and 'collision', the latter containing whether a collision occurred.
+   */
+  function maniCircleToCircle(leftCircle, rightCircle) {
+    if (!isCircle(leftCircle) || !isCircle(rightCircle)) {
+      throw "Invalid parameter";
+    }
+
+    const distanceBetween = {
+      x: rightCircle.position.x - leftCircle.position.x,
+      y: rightCircle.position.y - leftCircle.position.y
     };
 
-    /**
-     * 
-     * @param {{ position: {x: number, y: number}, radius: number }} leftCircle left circle.
-     * @param {{ position: {x: number, y: number}, radius: number }} rightCircle right circle.
-     * @returns {{ collision: boolean, distance: {x: number, y: number} }} object 
-     * containing the property 'distance' and 'collision', the latter containing whether a collision occurred.
-     */
-    var maniCircleToCircle = function (leftCircle, rightCircle) {
-        if (!isCircle(leftCircle) || !isCircle(rightCircle)) { throw "Invalid parameter"; }
-        
-        var distanceBetween = {
-            x: rightCircle.position.x - leftCircle.position.x,
-            y: rightCircle.position.y - leftCircle.position.y
-        };
+    const result = lengthSquared(distanceBetween) - Math.pow(leftCircle.radius + rightCircle.radius, 2);
+    if (0 > result) {
+      return { collision: false, distance: { x: 0, y: 0 } };
+    }
 
-        var result = lengthSquared(distanceBetween) - Math.pow(leftCircle.radius + rightCircle.radius, 2);
-        if (0 > result) { return { collision: false, distance: {x: 0, y: 0} }; }
-
-        // collision occurred generate manifest
-        var direction = unit(distanceBetween);
-
-        return {
-            collision: true,
-            distance: { x: direction.x * result, y: direction.y * result }
-        };
-    };
+    // collision occurred generate manifest
+    const direction = unit(distanceBetween);
 
     return {
-        boolCircleToCircle: boolCircleToCircle,
-        maniCircleToCircle: maniCircleToCircle
+      collision: true,
+      manifest: {
+        leftCircleDistance: { x: direction.x * result, y: direction.y * result },
+        rightCircleDistance: { x: (-direction.x) * result, y: (-direction.y) * result }
+      }
     };
+  };
+
+  return {
+    boolCircleToCircle: boolCircleToCircle,
+    maniCircleToCircle: maniCircleToCircle
+  };
 })();
 
-if (typeof module !== "undefined")
-{
-    module.exports = collisionManager;
+if (typeof module !== "undefined") {
+  module.exports = collisionManager;
 }
--- spec/p1-c00203250.js
+++ spec/p1-c00203250.js
@@ -1,50 +1,54 @@
 var path = require("path");
 var chai = require("chai");
 
-var collisionManager = require(path.join(__dirname, "..", "./p1-c00203250.js"));
+const collisionManager = require(path.join(__dirname, "..", "./p1-c00203250.js"));
 
-describe("collisionManager", function() {
-    "use strict";
+describe("collisionManager", function () {
+  "use strict";
 
-    it("exists", function () {
-        chai.expect(collisionManager).to.be.a("object");
-    });
+  it("exists", function () {
+    chai.expect(collisionManager).to.be.a("object");
+  });
 
-    it("circle to circle boolean collision detection", function () {
-        // assemble
-        var circle1 = {
-            position: {x: -9, y: 0},
-            radius: 10
-        };
-        var circle2 = {
-            position: {x: 10, y: 0},
-            radius: 10
-        };
+  it("circle to circle boolean collision detection", function () {
+    // assemble
+    const circle1 = {
+      position: { x: -9, y: 0 },
+      radius: 10
+    };
+    const circle2 = {
+      position: { x: 10, y: 0 },
+      radius: 10
+    };
 
-        // act
-        var result = collisionManager.boolCircleToCircle(circle1, circle2);
+    // act
+    const result = collisionManager.boolCircleToCircle(circle1, circle2);
 
-        // assert
-        chai.expect(result).to.be.a("boolean");
-        chai.expect(result).to.equal(true);
-    });
+    // assert
+    chai.expect(result).to.be.a("boolean");
+    chai.expect(result).to.equal(true);
+  });
 
-    it("circle to circle manifest collision detection", function () {
-        // assemble
-        var circle1 = {
-            position: {x: -9, y: 0},
-            radius: 10
-        };
-        var circle2 = {
-            position: {x: 10, y: 0},
-            radius: 10
-        };
+  it("circle to circle manifest collision detection", function () {
+    // assemble
+    const circle1 = {
+      position: { x: -9, y: 0 },
+      radius: 10
+    };
+    const circle2 = {
+      position: { x: 10, y: 0 },
+      radius: 10
+    };
+    const expectedResult = {
+      collision: true,
+      manifest: { distance: { x: 1, y: 0 } }
+    };
 
-        // act
-        var result = collisionManager.maniCircleToCircle(circle1, circle2);
+    // act
+    const result = collisionManager.maniCircleToCircle(circle1, circle2);
 
-        // assert
-        chai.expect(result).to.be.a("object");
-        chai.expect(result).to.eql({ distance: {x: 1, y: 0} });
-    });
+    // assert
+    chai.expect(result).to.be.a("object");
+    chai.expect(result).to.eql(expectedResult);
+  });
 });
