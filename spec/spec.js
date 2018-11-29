2c9f6bd6e16e30d5e182c84fdf2bb27935ed2844
Rafael Plugge <rafael.plugge@email.com>
2018-11-29 10:58:16 +0000

    Added tests for circle to aabb boolean checking.

--- spec/spec.js
+++ spec/spec.js
@@ -343,4 +343,80 @@ describe("collisionManager", function () {
       chai.expect(functionToThrow).to.throw(expectedErrorMessage);
     });
   });
+
+  describe(".boolCircleToAABB", function () {
+    it("exists", function () {
+      chai.expect(collisionManager.boolCircleToAABB).to.be.a("function");
+    });
+
+    it("should return true when circle and AABB are overlapping", function () {
+
+      // assemble
+      const circle = {
+        position: { x: 40, y: 20 },
+        radius: 20
+      };
+      const aabb = [
+        { x: 25, y: 10 },
+        { x: 50, y: 10 },
+        { x: 50, y: 30 },
+        { x: 25, y: 30 }
+      ];
+
+      // act
+      const result = collisionManager.boolCircleToAABB(circle, aabb);
+
+      // assert
+      chai.expect(result).to.be.a("boolean");
+      chai.expect(result).to.be.equal(true);
+    });
+
+    it("should return false when circle and AABB aren't overlapping", function () {
+
+      // assemble
+      const circle = {
+        position: { x: 100, y: 20 },
+        radius: 20
+      };
+      const aabb = [
+        { x: 25, y: 10 },
+        { x: 50, y: 10 },
+        { x: 50, y: 30 },
+        { x: 25, y: 30 }
+      ];
+
+      // act
+      const result = collisionManager.boolCircleToAABB(circle, aabb);
+
+      // assert
+      chai.expect(result).to.be.a("boolean");
+      chai.expect(result).to.equal(false);
+    });
+
+    it("should throw exception when incorrect parameters are passed", function () {
+      
+      // assemble
+      const circle = {};
+      const aabb = [{}];
+      const expectedErrorMessage = "Exception in function 'boolCircleToAABB' - Invalid parameter";
+
+      // act
+      const functionToThrow = collisionManager.boolCircleToAABB.bind(collisionManager, circle, aabb);
+
+      // assert
+      chai.expect(functionToThrow).to.throw(expectedErrorMessage);
+    });
+
+    it("should throw exception when no parameters are passed", function () {
+      
+      // assemble
+      const expectedErrorMessage = "Exception in function 'boolCircleToAABB' - Invalid parameter";
+
+      // act
+      const functionToThrow = collisionManager.boolCircleToAABB.bind(collisionManager);
+
+      // assert
+      chai.expect(functionToThrow).to.throw(expectedErrorMessage);
+    });
+  });
 });
